63d8341676e49d9e834311b2b0eacf685b0b32d6
yjjem <marksim0606@gmail.com>
2023-02-26 18:47:15 +0900

    feat: HTTPMethod 타입 추가

---
+++ 01-open-market/01-open-market/Data/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  01-open-market
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
